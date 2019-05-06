2ef1212bb6d324f0bbd29faaaccb449433916883
realplayer123 <39583785+realplayer123@users.noreply.github.com>
2019-05-06 15:03:57 +0700

    Update Rataan Suhu [n-inputs].asm

--- oak/Rataan Suhu [n-inputs].asm
+++ oak/Rataan Suhu [n-inputs].asm
@@ -4,7 +4,7 @@ extern scanf
   	 
 section .data
 	fmtScan db "%f", 0		; format input buat bil 
-	fmtN db "%d", 0		; format input n 
+	fmtN 	db "%d", 0		; format input n 
 	fmtPrint db "%.1f", 10, 0	; format print
 	sum dd 0.0  	 		; buat hitungan awalnya
       	 
@@ -20,7 +20,7 @@ main:
     push n			
 	push fmtN
 	call scanf
-    
+    add esp, 8
 	mov edi, [n]		; buat counter looping, jangan pake (loop) (ecx)
     	fld dword[sum]	; awalan hitungan 0.0 
     
@@ -37,17 +37,19 @@ looping:
     jne looping
 
     fidiv dword[n]	; dibagi n, kenapa pake fidiv dan bukan fdiv ? karena n itu integer bukan floating point.. jadinya pake fidiv bukan fdiv
-    fst qword[result]
+    fstp qword[result]
     
 
     push dword[result+4]	; print hasil
     push dword[result]
     push fmtPrint
     call printf
-
+	add esp, 8
 exit:
 	push 0
 	call fflush
+	add esp, 4
+	
 	mov eax, 1
 	mov ebx, 0
 	int 0x80
